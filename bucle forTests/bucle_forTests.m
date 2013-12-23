5bdf44795d55039e6dbc9ca66b6c3981297bff37
rober <gascon87@gmail.com>
2013-12-23 19:19:11 +0100

    Initial Commit

---
+++ bucle forTests/bucle_forTests.m
@@ -0,0 +1,34 @@
+//
+//  bucle_forTests.m
+//  bucle forTests
+//
+//  Created by rober on 23/12/13.
+//  Copyright (c) 2013 rogama. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface bucle_forTests : XCTestCase
+
+@end
+
+@implementation bucle_forTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
